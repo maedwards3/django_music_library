1f2dd0faaa93497a7e0ea651ce02a5f121fc55de
Mike Edwards <michael.edwards.4108@gmail.com>
2021-06-11 14:48:38 -0400

    Created 'Song' model. Migrated model changes and registered model.

--- music_library/music/models.py
+++ music_library/music/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=75)
+    artist = models.CharField(max_length=75)
+    album = models.CharField(max_length=75)
+    release_date = models.DateTimeField()
