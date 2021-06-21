c4feafb922078e60c6e077563ec1ff7ce69b71fb
Mike Edwards <michael.edwards.4108@gmail.com>
2021-06-21 11:37:08 -0400

    Added 'genre' to models.py class. Updated SongSerializer. Added corsheaders to settings.

--- music_library/music/models.py
+++ music_library/music/models.py
@@ -6,4 +6,5 @@ class Song(models.Model):
     title = models.CharField(max_length=75)
     artist = models.CharField(max_length=75)
     album = models.CharField(max_length=75)
-    release_date = models.DateTimeField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=75)
